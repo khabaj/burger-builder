07c6080b07a280426d137181b324c1262158d650
Krystian Habaj <khabaj@pgs-soft.com>
2018-01-19 09:12:37 +0100

    206. Navigating Back & To Next Page

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -87,30 +87,32 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
 
-        this.setState({ loading: true });
+        // this.setState({ loading: true });
 
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Jan Kowalski',
-                address: {
-                    street: 'qwerewrqwr',
-                    zipCode: '45123',
-                    country: 'Poland'
-                },
-                email: 'test@test.pl'
-            },
-            deliveryMethod: 'fastest'
-        };
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Jan Kowalski',
+        //         address: {
+        //             street: 'qwerewrqwr',
+        //             zipCode: '45123',
+        //             country: 'Poland'
+        //         },
+        //         email: 'test@test.pl'
+        //     },
+        //     deliveryMethod: 'fastest'
+        // };
 
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({ loading: false, purchasing: false });
-            })
-            .catch(error => {
-                this.setState({ loading: false, purchasing: false });
-            });
+        // axios.post('/orders.json', order)
+        //     .then(response => {
+        //         this.setState({ loading: false, purchasing: false });
+        //     })
+        //     .catch(error => {
+        //         this.setState({ loading: false, purchasing: false });
+        //     });
+
+        this.props.history.push('/checkout');
     }
 
     render() {
