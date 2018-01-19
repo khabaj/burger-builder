691b1b290a821bab116396f43a04a4390fb38214
Krystian Habaj <khabaj@pgs-soft.com>
2018-01-19 10:10:06 +0100

    208. Navigating to the Contact Data Component

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,6 +1,8 @@
 
 import React, { Component } from 'react';
 import CheckoutSummary from './../../components/Order/CheckoutSummary/CheckoutSummary';
+import { Route } from 'react-router-dom';
+import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
 
@@ -21,7 +23,7 @@ class Checkout extends Component {
             ingredients[param[0]] = +param[1];
         }
 
-        this.setState({ingredients: ingredients});
+        this.setState({ ingredients: ingredients });
     }
 
     checkoutCancelledHandler = () => {
@@ -29,16 +31,20 @@ class Checkout extends Component {
     }
 
     checkoutContinuedHandler = () => {
-        this.props.history.replace( '/checkout/contact-data');
+        this.props.history.replace('/checkout/contact-data');
     }
 
     render() {
         return (
             <div>
-                <CheckoutSummary 
-                    ingredients={this.state.ingredients} 
+                <CheckoutSummary
+                    ingredients={this.state.ingredients}
                     checkoutCancelled={this.checkoutCancelledHandler}
-                    checkoutContinued={this.checkoutContinuedHandler}/>
+                    checkoutContinued={this.checkoutContinuedHandler}
+                />
+                <Route
+                    path={this.props.match.path + '/contact-data'}
+                    component={ContactData} />
             </div>
         );
     }
