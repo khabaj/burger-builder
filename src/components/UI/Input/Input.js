ecc854b7dd8e72c5d3a796f778ba710cdb788701
Krystian Habaj <khabaj@pgs-soft.com>
2018-01-22 09:04:35 +0100

    222. Handling User Input

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -11,21 +11,40 @@ const input = (props) => {
                 className={classes.InputElement}
                 {...props.elementConfig}
                 value={props.value}
+                onChange={props.changed}
             />;
             break;
         case ('textarea'):
             inputElement = <textarea
                 className={classes.InputElement}
-                {...props.elementConfig} 
+                {...props.elementConfig}
                 value={props.value}
-                />;
+                onChange={props.changed}
+            />;
+            break;
+        case ('select'):
+            inputElement =
+                <select
+                    className={classes.InputElement}
+                    {...props.elementConfig}
+                    value={props.value}
+                    onChange={props.changed}>
+                    {props.elementConfig.options.map(option => (
+                        <option
+                            key={option.value}
+                            value={option.value}>
+                            {option.displayValue}
+                        </option>
+                    ))}}
+                </select>;
             break;
         default:
             inputElement = <input
                 className={classes.InputElement}
-                {...props.elementConfig} 
+                {...props.elementConfig}
                 value={props.value}
-                />;
+                onChange={props.changed}
+            />;
     }
 
     return (
